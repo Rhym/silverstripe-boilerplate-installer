2b4b759bd9d7de67846f664b0bdfffb3a0eafbba
Rhym <contact@ryanpotter.co.nz>
2015-02-04 11:27:43 +1300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'boilerplate/css/main.css': 'boilerplate/sass/main.scss'
+                }
+            },
+            cms: {
+                files: {
+                    'boilerplate/css/cms/css/cms.min.css': 'boilerplate/css/cms/sass/cms.scss'
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 3 versions']
+            },
+            dist: {
+                files: {
+                    'boilerplate/css/main.css': 'boilerplate/css/main.css'
+                }
+            },
+            cms: {
+                files: {
+                    'boilerplate/css/cms/css/cms.min.css': 'boilerplate/css/cms/css/cms.min.css'
+                }
+            }
+        },
+        cmq: {
+            options: {
+                log: false
+            },
+            dist: {
+                files: {
+                    'boilerplate/css/': ['boilerplate/css/main.css']
+                }
+            }
+        },
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: 'boilerplate/css/',
+                src: ['main.css'],
+                dest: 'boilerplate/css/',
+                ext: '.min.css'
+            }
+        },
+        watch: {
+            dist: {
+                files: ['boilerplate/sass/**/*.scss'],
+                tasks: ['sass:dist', 'autoprefixer:dist', 'cmq:dist', 'cssmin'],
+                options: {
+                    spawn: false
+                }
+            },
+            cms: {
+                files: ['boilerplate/css/cms/sass/**/*.scss'],
+                tasks: ['sass:cms', 'autoprefixer:cms'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+        //---------------------------------------
+        // CMS
+        //---------------------------------------
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-combine-media-queries');
+    grunt.registerTask('default',['watch:dist', 'autoprefixer:dist', 'cmq:dist', 'cssmin', 'watch']);
+    grunt.registerTask('cms',['watch:cms', 'autoprefixer:cms', 'watch:cms']);
+}
