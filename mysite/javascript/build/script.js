bfff56855116a4ff3be960daad3ab2146b017bfd
Rhym <contact@ryanpotter.co.nz>
2015-04-15 15:51:13 +1200

    Added naviagation to gallery pages

--- mysite/javascript/build/script.js
+++ mysite/javascript/build/script.js
@@ -1,13 +1,21 @@
 ;(function($) {
     $(document).ready(function(){
 
-        /*---------------------------------------------*\
-         * Carousel
-        \*---------------------------------------------*/
-
-        $('.owl-carousel').owlCarousel({
+        $('.carousel').owlCarousel({
             items: 1,
             dots: false
+        });
+
+        /*---------------------------------------------*\
+         * Gallery Carousel
+         \*---------------------------------------------*/
+
+        $('.gallery-carousel').owlCarousel({
+            items: 1,
+            dots: false,
+            //stagePadding: 200,
+            nav: true,
+            navText: ['<i class="fa fa-angle-left">', '<i class="fa fa-angle-right">']
         });
 
     });
