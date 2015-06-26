15edf9317b8e5a81a81a82fd53b3da1ce7e8060a
Rhym <contact@ryanpotter.co.nz>
2015-06-26 13:45:29 +1200

    Added the ability to set the cms menu's background via a config, and ustilising the cms-branding module

--- mysite/code/extensions/LeftAndMain.php
+++ mysite/code/extensions/LeftAndMain.php
@@ -5,28 +5,40 @@
  */
 class CMSBrandingLeftAndMainExtension extends LeftAndMainExtension {
 
+    private static $cms_menu_background = '#1b354c';
+
     public function init(){
         $config = SiteConfig::current_site_config();
 
-        /** Remove the Reports menu item in the CMS */
+        /**
+         * Remove the Reports menu item in the CMS
+         */
         if(!$config->ShowAssetAdmin) {
             CMSMenu::remove_menu_item('AssetAdmin');
         }
-
-        /** Remove the Security menu item in the CMS */
+        /**
+         * Remove the Security menu item in the CMS
+         */
         if(!$config->ShowSecurityAdmin) {
             CMSMenu::remove_menu_item('SecurityAdmin');
         }
-
-        /** Remove the Reports menu item in the CMS */
+        /**
+         * Remove the Reports menu item in the CMS
+         */
         if(!$config->ShowReportAdmin) {
             CMSMenu::remove_menu_item('ReportAdmin');
         }
-
-        /** Remove the Help Link in the CMS */
+        /**
+         * Remove the Help Link in the CMS
+         */
         if(!$config->ShowHelpLink) {
             CMSMenu::remove_menu_item('Help');
         }
+
+        /** Set the background colour of the cms sidebar menu. */
+        Requirements::customCSS(
+            '.cms-menu {background: '.$this->owner->stat('cms_menu_background').';}'
+        );
     }
 
 }
