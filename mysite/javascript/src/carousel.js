e3d47761dde8ac7fa563330aeb1ceb86d09df9ad
Rhym <contact@ryanpotter.co.nz>
2015-07-08 08:59:54 +1200

    Fixed bug with carousel

--- mysite/javascript/src/carousel.js
+++ mysite/javascript/src/carousel.js
@@ -3,16 +3,23 @@
      * Carousel
      ==========================================*/
 
-    var $carousel = $('.carousel');
+    var $carousel = $('.carousel--multiple');
     if ($carousel.length) {
         $carousel.owlCarousel({
             items: 1,
             dots: false,
-            nav: true,
-            navText: ['<i class="fa fa-angle-left">', '<i class="fa fa-angle-right">'],
-            mouseDrag: false,
-            navContainerClass: 'carousel__navigation',
-            navClass: [ 'carousel__navigation__item carousel__navigation__item--prev', 'carousel__navigation__item carousel__navigation__item--next' ]
+            nav: false,
+            mouseDrag: false
+        });
+        /** Custom navigation */
+        var owl = $carousel.data('owlCarousel');
+        $('.carousel-navigation__item--next').on('click', function(e){
+            e.preventDefault();
+            owl.next();
+        });
+        $('.carousel-navigation__item--prev').on('click', function(e){
+            e.preventDefault();
+            owl.prev();
         });
     }
 })(jQuery);
