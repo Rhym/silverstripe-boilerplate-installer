bf995a9d4a812248526fae383c4a4f317d24b9c1
Rhym <contact@ryanpotter.co.nz>
2015-08-10 15:34:32 +1200

    MINOR fixed comment header in waypoints to be consistant with all others

--- mysite/javascript/main.js
+++ mysite/javascript/main.js
@@ -17218,23 +17218,22 @@ require('./waypoints.js');
 })(jQuery);
 
 },{}],12:[function(require,module,exports){
-//;
-//(function ($) {
-//  $(document).ready(function () {
-//
-//    /*---------------------------------------------*\
-//     * Animation - https://github.com/imakewebthings/waypoints
-//     \*---------------------------------------------*/
-//
-//    $('.animated').waypoint(function () {
-//      var animation = $(this).data('animation');
-//      $(this).addClass(animation);
-//    }, {
-//      context: '.content-wrap',
-//      offset: '75%'
-//    });
-//
-//  });
-//})(jQuery);
+;
+(function ($) {
+  $(document).ready(function () {
+    /** -----------------------------------------
+     * Animation - https://github.com/imakewebthings/waypoints
+     * ----------------------------------------*/
+
+    $('.animated').waypoint(function () {
+      var animation = $(this).data('animation');
+      $(this).addClass(animation);
+    }, {
+      context: '.content-wrap',
+      offset: '75%'
+    });
+
+  });
+})(jQuery);
 
 },{}]},{},[10]);
--- mysite/javascript/src/waypoints.js
+++ mysite/javascript/src/waypoints.js
@@ -1,18 +1,17 @@
-//;
-//(function ($) {
-//  $(document).ready(function () {
-//
-//    /*---------------------------------------------*\
-//     * Animation - https://github.com/imakewebthings/waypoints
-//     \*---------------------------------------------*/
-//
-//    $('.animated').waypoint(function () {
-//      var animation = $(this).data('animation');
-//      $(this).addClass(animation);
-//    }, {
-//      context: '.content-wrap',
-//      offset: '75%'
-//    });
-//
-//  });
-//})(jQuery);
+;
+(function ($) {
+  $(document).ready(function () {
+    /** -----------------------------------------
+     * Animation - https://github.com/imakewebthings/waypoints
+     * ----------------------------------------*/
+
+    $('.animated').waypoint(function () {
+      var animation = $(this).data('animation');
+      $(this).addClass(animation);
+    }, {
+      context: '.content-wrap',
+      offset: '75%'
+    });
+
+  });
+})(jQuery);
